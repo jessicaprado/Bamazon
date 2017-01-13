c4bb5d36c644a726e12464d4d485a097538f86ae
Jessica Prado <jessica.marie.prado@gmail.com>
2017-01-12 20:49:27 -0500

    Manager can view inventory table

--- BamazonManager.js
+++ BamazonManager.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var consoleTable = require('console.table');
 
 var connection = mysql.createConnection({
     host    : "localhost",
@@ -16,3 +17,43 @@ connection.connect(function(err) {
     }
     console.log("connected as id " + connection.threadId)
 })
+
+var displayInventory = function () {
+    var inventory = [];
+    connection.query('SELECT * FROM bamazon.inventory', function(err, res) {
+        var itemsArray = [];
+        for (var i = 0; i < res.length; i++) {
+            itemsArray.push([res[i].itemID, res[i].ProductName, res[i].Price, res[i].StockQuantity]);
+        }
+        inventory.push(itemsArray);
+        for (var i = 0; i < inventory.length; i++) {
+            console.table(['ID', 'Name', 'Price', 'Stock'], inventory[i])
+        }
+    })
+}
+
+var start = function() {
+    connection.query('SELECT * FROM bamazon.inventory', function(res) {
+        inquirer.prompt([
+          {
+            type: 'list',
+            name: 'action',
+            message: 'What do you want to do?',
+            choices: [
+              'View Products for Sale',
+              'View Low Inventory',
+              'Add to Inventory',
+              'Add New Product'
+            ]
+          },
+        ]).then(function (answers) {
+          switch(answers.action) {
+            case 'View Products for Sale':
+                displayInventory();
+            break;
+          }
+        });
+    })
+}
+
+start();
